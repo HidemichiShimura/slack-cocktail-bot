71a43adaaa3673e0f5000647742e5352e19a1b85
“Hidemichi <hidemichi.shimura@gmail.com>
2022-11-14 23:35:41 -0800

    add POST request handling for the URL verification

--- src/server.js
+++ src/server.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.listen(process.env.PORT || 5000, () => {
     console.log("Server is runnning");
+});
+
+app.post("/", (req, res) => {
+    // Respond to the URL verification
+    res.set({
+        'Content-Type': 'text/plain'
+    });
+    res.send(req.body.challenge);
 });
