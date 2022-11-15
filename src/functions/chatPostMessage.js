affb744977c852dde64cdb751f1ca2b2e89bf86f
“Hidemichi <hidemichi.shimura@gmail.com>
2022-11-15 00:14:14 -0800

    add a function for bot to post a message

---
+++ src/functions/chatPostMessage.js
@@ -0,0 +1,34 @@
+const dotenv = require("dotenv");
+const https = require("https");
+
+dotenv.config();
+
+function chatPostMessage(text, channel) {
+    const options = {
+        hostname: "slack.com",
+        port: 443,
+        path: "/api/chat.postMessage",
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": process.env.AUTH
+        }
+    };
+
+    const body = JSON.stringify({
+        // Set the input text here
+        text: text,
+        channel: channel
+    });
+    
+    const request = https.request(options, (res) => {
+        res.on("data", (chunk) => {
+            console.log(JSON.parse(chunk));
+        });
+     });
+    
+    request.write(body);
+    request.end();
+}
+
+module.exports = chatPostMessage;
